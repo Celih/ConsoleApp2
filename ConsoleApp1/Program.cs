86fd502629fc8cf694bce44865d9f981e9f1f581
Celih <remigravelaine@gmail.com>
2017-10-04 18:01:09 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceReference1.Service1Client aze = new ServiceReference1.Service1Client();
+            string str = aze.GetData(10);
+        }
+    }
+}
